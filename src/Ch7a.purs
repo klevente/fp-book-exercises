8fe830b205d3427fd08f182c1130fa6b46dcb337
Levente Krizsan <lewalevi@gmail.com>
2021-10-31 10:14:15 +0100

    feat(ch7): derive Eq, Ord, Show for Maybe and Either

--- src/Ch7a.purs
+++ src/Ch7a.purs
@@ -1,16 +1,18 @@
 module Ch7a where
 
-import Prelude (Unit, discard, (==), ($), (<), (>), (<=), (||), (<>))
+import Prelude (Unit, discard, ($), (<), (<=), (==), (>), (>=))
 
 import Data.Eq (class Eq)
-import Data.Ord (class Ord, Ordering(..), compare)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+import Data.Ord (class Ord)
 import Data.Show (class Show, show)
 import Effect (Effect)
 import Effect.Console (log)
 
 data Maybe a = Nothing | Just a
 
-instance eqMaybe :: Eq a => Eq (Maybe a) where
+{-instance eqMaybe :: Eq a => Eq (Maybe a) where
     eq Nothing Nothing   = true
     eq (Just x) (Just y) = x == y
     eq _ _               = false
@@ -28,7 +30,23 @@ infixl 4 greaterThanOrEq as >=
 
 instance showMaybe :: Show a => Show (Maybe a) where
     show Nothing = "Nothing"
-    show (Just x)  = "(Just " <> show x <> ")"
+    show (Just x)  = "(Just " <> show x <> ")"-}
+
+-- automatic versions for Eq, Ord, Show
+derive instance eqMaybe :: Eq a => Eq (Maybe a)
+derive instance ordMaybe :: Ord a => Ord (Maybe a)
+derive instance genericMaybe :: Generic (Maybe a) _ -- second type parameter is `_`, needed for the compiler
+instance showMaybe :: Show a => Show (Maybe a) where
+    show = genericShow
+
+data Either a b = Left a | Right b
+derive instance eqEither :: (Eq a, Eq b) => Eq (Either a b)
+derive instance ordEither :: (Ord a, Ord b) => Ord (Either a b)
+derive instance genericEither :: Generic (Either a b) _
+instance showEither :: (Show a, Show b) => Show (Either a b) where
+    show = genericShow
+
+type MyEitherVar = Either String (Maybe Int) -- create type alias so variables of this type can be created
 
 test :: Effect Unit
 test = do
@@ -53,3 +71,7 @@ test = do
     log $ show $ Just "abc"
     log $ show $ (Nothing :: Maybe Unit)
     log ""
+
+    log "Either Show:"
+    log $ show $ (Left "left" :: Either _ Unit)
+    log $ show $ (Right (Just 42) :: Either Unit _)

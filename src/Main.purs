3b24397a7acc6a3c59db8ea0b45e7753343d50d6
Levente Krizsan <lewalevi@gmail.com>
2021-10-31 10:15:00 +0100

    refactor(ch7): add Ch7b file

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,8 @@ import Prelude
 import Effect (Effect)
 
 -- import Ch5 as Ch5
-import Ch7a as Ch7a
+-- import Ch7a as Ch7a
+import Ch7b as Ch7b
 
 main :: Effect Unit
-main = Ch7a.test
+main = Ch7b.test

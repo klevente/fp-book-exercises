bcde3d9449fad0b9f0eea90111dd1a00f18824db
Levente Krizsan <lewalevi@gmail.com>
2021-11-09 13:13:34 +0100

    feat(ch9): define Mod4, implement Semigroup, Monoid and Group for it

--- src/Ch9.purs
+++ src/Ch9.purs
@@ -21,7 +21,6 @@ class Semigroup a <= Monoid a where
 data AndBool = AFalse | ATrue
 derive instance eqAndBool :: Eq AndBool
 derive instance genericAndBool :: Generic AndBool _
-
 instance showAndBool :: Show AndBool where
     show = genericShow
 
@@ -49,7 +48,6 @@ verifyAndBoolMonoid = do
 data OrBool = OFalse | OTrue
 derive instance eqOrBool :: Eq OrBool
 derive instance genericOrBool :: Generic OrBool _
-
 instance showOrBool :: Show OrBool where
     show = genericShow
 
@@ -73,6 +71,56 @@ verifyOrBoolMonoid = do
     log $ show $ mempty <> OTrue == OTrue <> mempty && OTrue <> mempty == OTrue
     log $ show $ mempty <> OFalse == OFalse <> mempty && OFalse <>mempty == OFalse
 
+data Mod4 = Zero | One | Two | Three
+derive instance eqMod4 :: Eq Mod4
+derive instance genericMod4 :: Generic Mod4 _
+instance showMod4 :: Show Mod4 where
+    show = genericShow
+
+-- have to write it manually for all inputs
+instance semigroupMod4 :: Semigroup Mod4 where
+    append Zero y = y
+    append x Zero = x
+
+    append One One   = Two
+    append One Two   = Three
+    append One Three = Zero
+
+    append Two One   = Three
+    append Two Two   = Zero
+    append Two Three = One
+
+    append Three One   = Zero
+    append Three Two   = One
+    append Three Three = Two
+
+instance monoidMod4 :: Monoid Mod4 where
+    mempty = Zero
+
+class Monoid a <= Group a where
+    ginverse :: a -> a
+
+instance groupMod4 :: Group Mod4 where
+    ginverse Zero  = Zero
+    ginverse One   = Three
+    ginverse Two   = Two
+    ginverse Three = One
+
+class Semigroup g <= Commutative g
+
+-- since the addition operator for `Mod4` is commutative, an instance can be created for it
+instance commutativeMod4 :: Commutative Mod4
+
+verifyMod4Semigroup :: Effect Unit
+verifyMod4Semigroup = do
+    log "Verifying Mod4 Semigroup Laws (1 test)"
+    log $ show $ One <> (Two <> Three) == (One <> Two) <> Three
+
+verifyMod4Monoid :: Effect Unit
+verifyMod4Monoid = do
+    log "Verifying Mod4 Monoid Laws (1 test)"
+    log $ show $ mempty <> One == One <> mempty && One <> mempty == One
+
 test :: Effect Unit
 test = do
     log "AndBool Semigroup:"
@@ -89,3 +137,6 @@ test = do
 
     verifyOrBoolSemigroup
     verifyOrBoolMonoid
+
+    verifyMod4Semigroup
+    verifyMod4Monoid

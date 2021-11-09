7407ca55d114a33728abb8fc8263361117a7e763
Levente Krizsan <lewalevi@gmail.com>
2021-11-09 15:13:18 +0100

    feat(ch11): implement max

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -1,6 +1,6 @@
 module Ch11 where
 
-import Prelude (Unit, show, type (~>), ($), discard)
+import Prelude (class Ord, Unit, show, negate, discard, otherwise, type (~>), ($), (>))
 
 import Data.List (List(..), (:), foldl)
 import Effect (Effect)
@@ -9,8 +9,15 @@ import Effect.Console (log)
 reverse :: List ~> List
 reverse = foldl (\rl x -> x : rl) Nil
 
+max :: ∀ a. Ord a => a -> a -> a
+max x y | x > y     = x
+        | otherwise = y
+
 test :: Effect Unit
 test = do
     log "reverse:"
     log $ show $ reverse (10 : 20 : 30 : Nil)
+
+    log $ show $ max (-1) 99
+    log $ show $ max "aa" "z"
 

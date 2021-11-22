acee63d6e4073e9dc5da05aed0106798bd3b3536
Levente Krizsan <lewalevi@gmail.com>
2021-11-22 11:13:59 +0100

    feat(ch15): implement even and odd

--- src/Ch15.purs
+++ src/Ch15.purs
@@ -2,9 +2,18 @@ module Ch15 where
 
 import Prelude
 
+import Data.Int.Bits ((.&.))
 import Effect (Effect)
 import Effect.Console (log)
 
+even :: Int -> Boolean
+even x = x .&. 1 == 0
+
+odd :: Int -> Boolean
+odd x = x .&. 1 == 1
+
 test :: Effect Unit
 test = do
-    log "placeholder"
+    log "odd"
+    log $ show $ odd 0
+    log $ show $ odd 1

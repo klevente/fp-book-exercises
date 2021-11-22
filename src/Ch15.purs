74bdfced2e2a9d148fead4bc79077e0fab29c0ea
Levente Krizsan <lewalevi@gmail.com>
2021-11-22 17:41:38 +0100

    feat(ch15): implement contravariantPredicate

--- src/Ch15.purs
+++ src/Ch15.purs
@@ -2,6 +2,7 @@ module Ch15 where
 
 import Prelude
 
+import Data.Functor.Contravariant (class Contravariant, cmap, (>$<))
 import Data.Int.Bits ((.&.))
 import Effect (Effect)
 import Effect.Console (log)
@@ -17,6 +18,12 @@ data Predicate a = Predicate (a -> Boolean)
 runPredicate :: ∀ a. Predicate a -> a -> Boolean
 runPredicate (Predicate f) = f
 
+-- `Contravariant` maps the supplied function `f` over the `Predicate`,
+-- augmenting the input value before the `Predicate`'s function `g` is executed
+-- the resulting function is then re-wrapped inside a `Predicate`
+instance contravariantPredicate :: Contravariant Predicate where
+    cmap f (Predicate g) = Predicate $ g <<< f
+
 test :: Effect Unit
 test = do
     log "odd:"
@@ -26,3 +33,9 @@ test = do
     log "runPredicate:"
     log $ show $ runPredicate (Predicate odd) $ 10
     log $ show $ runPredicate (Predicate odd) $ 11
+
+    log "Contravariant Predicate:"
+    log $ show $ runPredicate (cmap (_ + 1) (Predicate odd)) 10
+    log $ show $ runPredicate (cmap (_ + 2) (Predicate odd)) 10
+    log $ show $ runPredicate ((_ + 1) >$< (Predicate odd)) 10
+    log $ show $ runPredicate ((_ + 2) >$< (Predicate odd)) 10

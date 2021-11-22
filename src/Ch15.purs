3c6dcd49d77556a8e1b7129b03a30cadc22e8d24
Levente Krizsan <lewalevi@gmail.com>
2021-11-22 11:27:35 +0100

    feat(ch15): implement Predicate, runPredicate

--- src/Ch15.purs
+++ src/Ch15.purs
@@ -12,8 +12,17 @@ even x = x .&. 1 == 0
 odd :: Int -> Boolean
 odd x = x .&. 1 == 1
 
+data Predicate a = Predicate (a -> Boolean)
+
+runPredicate :: ∀ a. Predicate a -> a -> Boolean
+runPredicate (Predicate f) = f
+
 test :: Effect Unit
 test = do
-    log "odd"
+    log "odd:"
     log $ show $ odd 0
     log $ show $ odd 1
+
+    log "runPredicate:"
+    log $ show $ runPredicate (Predicate odd) $ 10
+    log $ show $ runPredicate (Predicate odd) $ 11

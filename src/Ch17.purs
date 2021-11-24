83f556b4477bd14961e94808f8fba3bd5d6a2431
Levente Krizsan <lewalevi@gmail.com>
2021-11-24 21:50:13 +0100

    feat(ch17): implement Either and all its Functor-like instances

--- src/Ch17.purs
+++ src/Ch17.purs
@@ -2,6 +2,7 @@ module Ch17 where
 
 import Prelude
 
+import Data.Bifunctor (class Bifunctor)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
@@ -26,9 +27,36 @@ instance applicativeMaybe :: Applicative Maybe where
     -- wrap the supplied value into the context, using `Just`; written in point-free, where the value is implied
     pure = Just
 
+data Either a b = Left a | Right b
+
+derive instance genericEither :: Generic (Either a b) _
+instance showEither :: (Show a, Show b) => Show (Either a b) where
+    show = genericShow
+derive instance eqEither :: (Eq a, Eq b) => Eq (Either a b)
+derive instance ordEither :: (Ord a, Ord b) => Ord (Either a b)
+derive instance functorEither :: Functor (Either a) -- here the compiler generates the implementation!
+instance bifunctorEither :: Bifunctor Either where
+    bimap f _ (Left x) = Left $ f x
+    bimap _ g (Right y) = Right $ g y
+instance applyEither :: Apply (Either a) where
+    apply (Right f) x  = f <$> x
+    apply (Left err) _ = Left err
+instance applicativeEither :: Applicative (Either a) where
+    pure = Right
+
 test :: Effect Unit
 test = do
     log "Maybe Functor, Apply, Applicative:"
     log $ show $ (+) <$> Just 21 <*> Just 21
     log $ show $ (*) <$> pure 2 <*> (pure 21 :: Maybe Int)
     log $ show $ pure (+) <*> Just 17 <*> Just 25
+
+    log "Either Applicative Laws:"
+    log "Associative composition: (<<<) <$> u <*> v <*> w = u <*> (v <*> w)"
+    log $ show $ ((<<<) <$> pure identity <*> pure identity <*> pure 1) == (pure identity <*> (pure identity <*> pure 1) :: Either Unit Int)
+    log "Identity: pure identity <*> x = x"
+    log $ show $ (pure identity <*> pure 1) == (pure 1 :: Either Unit Int)
+    log "Homomorphism: pure (f x) = pure f <*> pure x"
+    log $ show $ pure (negate 1) == (pure negate <*> pure 1 :: Either Unit Int)
+    log "Interchange: u <*> pure x = pure (_ $ x) <*> u"
+    log $ show $ (pure negate <*> pure 1) == (pure (_ $ 1) <*> pure negate :: Either Unit Int)

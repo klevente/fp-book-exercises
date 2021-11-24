f0748e7973a9ae00773a010d16701db710d529d8
Levente Krizsan <lewalevi@gmail.com>
2021-11-24 21:35:59 +0100

    feat(ch17): implement Maybe, functorMaybe, applyMaybe, applicativeMaybe

--- src/Ch17.purs
+++ src/Ch17.purs
@@ -2,9 +2,33 @@ module Ch17 where
 
 import Prelude
 
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
 
+data Maybe a = Nothing | Just a
+
+derive instance genericMaybe :: Generic (Maybe a) _
+instance showMaybe :: Show a => Show (Maybe a) where
+    show = genericShow
+instance functorMaybe :: Functor Maybe where
+    -- if the value is present, execute the function and wrap the result in `Just`
+    map f (Just x) = Just $ f x
+    -- if there is no value available, return `Nothing`
+    map _ Nothing  = Nothing
+instance applyMaybe :: Apply Maybe where
+    -- use `map` (`<$>`) for handling both cases where `x` is `Just y` and `Nothing`
+    apply (Just f) x = f <$> x
+    -- if there is no function, return `Nothing`
+    apply Nothing _         = Nothing
+instance applicativeMaybe :: Applicative Maybe where
+    -- wrap the supplied value into the context, using `Just`; written in point-free, where the value is implied
+    pure = Just
+
 test :: Effect Unit
 test = do
-    log "placeholder"
+    log "Maybe Functor, Apply, Applicative:"
+    log $ show $ (+) <$> Just 21 <*> Just 21
+    log $ show $ (*) <$> pure 2 <*> (pure 21 :: Maybe Int)
+    log $ show $ pure (+) <*> Just 17 <*> Just 25

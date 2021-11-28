c0cbaf74d75a6ece3fc88e4f0e4ecb3f7ea72a77
Levente Krizsan <lewalevi@gmail.com>
2021-11-28 17:48:49 +0100

    feat(ch19): implement bindParser, fix up applyParser

--- src/MonadicParser.purs
+++ src/MonadicParser.purs
@@ -34,26 +34,50 @@ instance functorParser :: Functor (Parser e) where
     -- for double `map`ping, this is usually the preferred syntax, it is important to have `(map f)` in `()`s as
     -- the second `map` (`<$>`) expects a function as its first argument, which in this case is the `map`ped version of `f`
     -- with regular function invocations, it looks like this: `map (map f) (g s)`, which is a bit clearer
-    map f (Parser g) = Parser \s -> (map f) <$> (g s)
-    -- using the `parse` function: ` map f p = Parser \s -> map f <$> parse p s`, which is a bit clearer
+    map f p = Parser \s -> map f <$> parse p s
+    -- without using the `parse` function: `map f (Parser g) = Parser \s -> (map f) <$> (g s)`
 
 -- in Haskell/PureScript, you think for 2 hours then code for 2 minutes
 -- this function can be made simpler using `Monad`s, as `Either` is one
 instance applyParser :: Apply (Parser e) where
-    apply :: ∀ a b. Parser e (a -> b) -> Parser e a -> Parser e b
+    {-apply :: ∀ a b. Parser e (a -> b) -> Parser e a -> Parser e b
     -- apply the first parser's function (`f`) to `s` and check the result
-    apply (Parser f) (Parser g) = Parser \s -> case f s of -- using `parse`: `case parse p1 s of`
+    apply p1 p2 = Parser \s -> case parse p1 s of
         Left err -> Left err -- return an error if the first `Parser` fails
-        -- if successful, apply the second parser's function (`g`) to the remainder and check the result
-        Right (Tuple s1 h) -> case g s1 of -- here `h` is the mapping function of type `a -> b` (`parse` version: `case parse p2 s1 of`)
+        -- if successful, apply the second parser's function to the remainder and check the result
+        Right (Tuple s1 h) -> case parse p2 s1 of -- here `h` is the mapping function of type `a -> b`
             Left err -> Left err -- return an error if the second `Parser` fails
-            Right (Tuple s2 x) -> Right $ Tuple s2 (h x) -- apply the mapping `h` to the value `x`
+            Right (Tuple s2 x) -> Right $ Tuple s2 (h x) -- apply the mapping `h` to the value `x`-}
+    apply :: ∀ a b. Parser e (a -> b) -> Parser e a -> Parser e b
+    apply p1 p2 = Parser \s -> do
+        -- execute `p1` on `s` and destructure the result, the `h` returned is the mapping function
+        Tuple s1 h <- parse p1 s
+        -- exeute `p2` on `s1` and destructure the result, `x` is the value that needs to be mapped
+        Tuple s2 x <- parse p2 s1
+        -- construct the return value, which consists of the rest of the input `s2` and the mapped value `h x`
+        -- as everything is happening in the `Either` monad, `pure` is needed to return an `Either` instance (`Right`)
+        pure $ Tuple s2 $ h x
+    -- can also be written as: `import Control.Monad (ap) (...) apply = ap`, which does this:
+    -- ap ff fx = do
+    --     f <- ff
+    --     x <- fx
+    --     pure $ f x
 
 instance applicativeParser :: Applicative (Parser e) where
     -- return a successful parse of `x` without consuming any characters from the supplied input `s`
     pure x = Parser \s -> pure $ Tuple s x -- the `pure` here is for `Either`
 -- in all 3 instances, the pattern `Parser \s -> ...` is common; it will be more apparent when coding `Monad`s,
 -- as this pattern is prevalent when dealing with a `Functor` containing a function
+
+instance bindParser :: Bind (Parser e) where
+    -- this function works in the `Either` monad, meaning that every computation inside the `do` block
+    -- must return `Either`, which makes it so only the happy path needs to be coded, as `bindEither` handles all the errors
+    bind p f = Parser \s -> do
+        -- first, run the `Parser` `p` on the input `s` and destructure the result
+        Tuple s1 x <- parse p s
+        -- the remainder of the string (`s1`) is parsed using `f x`, which is of type `Parser b`
+        -- this is because `f` is a monadic function, meaning it is: `f :: a -> Parser b` and `x :: a`
+        parse (f x) s1
 
 -- my version returns the result unwrapped in an `Either`
 myParse :: ∀ e a. ParserError e => Parser e a -> String -> Either e a

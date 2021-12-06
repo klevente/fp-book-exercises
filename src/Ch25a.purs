9844446ec0d651d2795cc77dd5ebe6b7434b689b
Levente Krizsan <lewalevi@gmail.com>
2021-12-06 19:20:55 +0100

    feat(ch25): add data model

--- src/Ch25a.purs
+++ src/Ch25a.purs
@@ -5,6 +5,30 @@ import Prelude
 import Effect (Effect)
 import Effect.Class.Console (log)
 
+newtype Centimeters = Centimeters Number
+newtype Kilograms = Kilograms Number
+newtype Years = Years Int
+type Personal =
+    { height :: Centimeters
+    , weight :: Kilograms
+    , age :: Years
+    }
+newtype GPA = GPA Number
+data Grade = Preschool | Kindergarten | Grade Int | High Int | College Int
+type Student =
+    { grade :: Grade
+    , teacher :: Teacher
+    , gpa :: GPA
+    , personal :: Personal
+    }
+data TeachingStatus = Student | Probationary | NonTenured | Tenured
+type Teacher =
+    { grades :: Array Grade
+    , numberOfStudents :: Int
+    , personal :: Personal
+    , status :: TeachingStatus
+    }
+
 test :: Effect Unit
 test = do
     log "placeholder"

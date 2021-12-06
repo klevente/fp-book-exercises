12048fe74c2b9ce4149a043ce75172bb5fa17b56
Levente Krizsan <lewalevi@gmail.com>
2021-12-06 20:00:16 +0100

    feat(ch25): implement json decode/encode for data types

--- src/Ch25a.purs
+++ src/Ch25a.purs
@@ -2,8 +2,11 @@ module Ch25a where
 
 import Prelude
 
+import Data.Generic.Rep (class Generic)
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Foreign.Generic (genericDecode, genericEncode, encodeJSON)
+import Foreign.Generic.Class (class Encode, class Decode, defaultOptions)
 
 newtype Centimeters = Centimeters Number
 newtype Kilograms = Kilograms Number
@@ -29,6 +32,42 @@ type Teacher =
     , status :: TeachingStatus
     }
 
+derive instance genericCentimeters :: Generic Centimeters _
+-- use `derive newtype` to leverage the underlying `Number`'s implementation, prints: 12.34, which is enough for this use-case
+derive newtype instance encodeCentimeters :: Encode Centimeters
+derive newtype instance decodeCentimeters :: Decode Centimeters
+-- use `Generic` to implement the codec, prints: `{"contents": 12.34,"tag":"Centimeters"}`, which preserves type safety, but is too complicated for here
+{-instance encodeCentimeters :: Encode Centimeters where
+    encode = genericEncode defaultOptions
+instance decodeCentimeters :: Decode Centimeters where
+    decode = genericDecode defaultOptions-}
+
+derive instance genericKilograms :: Generic Kilograms _
+derive newtype instance encodeKilograms :: Encode Kilograms
+derive newtype instance decodeKilograms :: Decode Kilograms
+
+derive instance genericYears :: Generic Years _
+derive newtype instance encodeYears :: Encode Years
+derive newtype instance decodeYears :: Decode Years
+
+derive instance genericGPA :: Generic GPA _
+derive newtype instance encodeGPA :: Encode GPA
+derive newtype instance decodeGPA :: Decode GPA
+
+derive instance genericGrade :: Generic Grade _
+instance encodeGrade :: Encode Grade where
+    encode = genericEncode defaultOptions
+instance decodeGrade :: Decode Grade where
+    decode = genericDecode defaultOptions
+
+derive instance genericTeachingStatus :: Generic TeachingStatus _
+instance encodeTeachingStatus :: Encode TeachingStatus where
+    encode = genericEncode defaultOptions
+instance decodeTeachingStatus :: Decode TeachingStatus where
+    decode = genericDecode defaultOptions
+
+-- all `Record`s are already `Encode` and `Decode` as long as all their fields are, so no further instantiations are necessary
+
 test :: Effect Unit
 test = do
-    log "placeholder"
+    log $ encodeJSON $ Centimeters 12.34
